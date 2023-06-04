ed251bd60eb8f62a8cea4651bcb206984bc629f5
jeonghunbak <iiiiilllll@nate.com>
2023-06-04 22:32:55 +0900

    Blog Example - create controller

---
+++ src/main/java/com/cloud/springboot/controller/BlogApiController.java
@@ -0,0 +1,24 @@
+package com.cloud.springboot.controller;
+
+import com.cloud.springboot.domain.Article;
+import com.cloud.springboot.dto.AddArticleRequest;
+import com.cloud.springboot.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
