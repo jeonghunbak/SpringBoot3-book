c8fbc87730cf34c431d88b295428b8fa91c78390
jeonghunbak <iiiiilllll@nate.com>
2023-06-05 22:26:25 +0900

    Blog Example - test controller

--- src/test/java/com/cloud/springboot/controller/BlogApiControllerTest.java
+++ src/test/java/com/cloud/springboot/controller/BlogApiControllerTest.java
@@ -1,14 +1,27 @@
 package com.cloud.springboot.controller;
 
+import com.cloud.springboot.domain.Article;
+import com.cloud.springboot.dto.AddArticleRequest;
 import com.cloud.springboot.repository.BlogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,10 +35,42 @@ class BlogApiControllerTest {
 
     @Autowired
     BlogRepository blogRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @BeforeEach
     public void mockMvcSetup(){
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
         blogRepository.deleteAll();
     }
+
+    @DisplayName("add Article")
+    @Test
+    public void addArticle() throws Exception {
+        //given
+        final String url = "/api/articles";
+        final String title = "Spring boot title";
+        final String content = "Cloud";
+        final AddArticleRequest request = new AddArticleRequest(title, content);
+
+        //Serialize JSON
+        final String requestBody = objectMapper.writeValueAsString(request);
+
+        //given
+        //요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        //then
+        result.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);// 크기가 1인지
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
 }
