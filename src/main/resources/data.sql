d3017a99dd3883bc06931a76a8cc7eef7b1c4f02
jeonghunbak <iiiiilllll@nate.com>
2023-06-06 23:19:13 +0900

    Blog Example - add sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article (title, content) VALUES ('title_1', 'content_1');
+INSERT INTO article (title, content) VALUES ('title_2', 'content_2');
+INSERT INTO article (title, content) VALUES ('title_3', 'content_3');
+INSERT INTO article (title, content) VALUES ('title_4', 'content_4');
