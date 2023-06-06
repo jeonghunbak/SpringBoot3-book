8b83ba2be1e744139f19104bed5e60cbee95574f
jeonghunbak <iiiiilllll@nate.com>
2023-06-06 17:54:59 +0900

    Blog Example - test controller

--- src/main/java/com/cloud/springboot/controller/BlogApiController.java
+++ src/main/java/com/cloud/springboot/controller/BlogApiController.java
@@ -6,6 +6,7 @@ import com.cloud.springboot.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
+    @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
         Article savedArticle = blogService.save(request);
 
