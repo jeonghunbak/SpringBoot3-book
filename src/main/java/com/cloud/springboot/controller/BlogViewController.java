c8f23debafe541e0cf9d9d12b72846b2594cd87a
jeonghunbak <iiiiilllll@nate.com>
2023-06-10 20:06:10 +0900

    Blog Example - List Commit

---
+++ src/main/java/com/cloud/springboot/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.cloud.springboot.controller;
+
+import com.cloud.springboot.dto.ArticleListViewResponse;
+import com.cloud.springboot.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model){
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("list", articles);
+
+        return "articleList";
+    }
+}
