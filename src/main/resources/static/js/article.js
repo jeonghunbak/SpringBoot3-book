8c697e0c3d2d1ffc3702eca840b4575fef0efdb3
jeonghunbak <iiiiilllll@nate.com>
2023-06-11 21:49:59 +0900

    Blog Example - Modify Commit

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -6,11 +6,36 @@ if(deleteButton){
         let id = document.getElementById('article-id').value;
 
         fetch(`/api/articles/${id}`, {
-            method: 'DELETE'
+            method : 'DELETE'
         })
         .then(()=>{
             alert("DELETE SUCCESS");
             location.replace('/articles');
+        });
+    });
+}
+
+//modify
+const modifyButton = document.getElementById('modify-btn');
+
+if(modifyButton){
+    modifyButton.addEventListener('click', ev => {
+        let param = new URLSearchParams(location.search);
+        let id = param.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method : 'PUT',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                'title' : document.getElementById('title').value,
+                'content' : document.getElementById('content').value
+            }),
+        })
+        .then(()=>{
+            alert('Modify Success');
+            location.replace(`/articles/${id}`);
         });
     });
 }
