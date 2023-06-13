19d11c5355e87d20666aec993a6653475a060d62
jeonghunbak <iiiiilllll@nate.com>
2023-06-13 20:36:34 +0900

    Blog Example - create User Repository

---
+++ src/main/java/com/cloud/springboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cloud.springboot.repository;
+
+import com.cloud.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
