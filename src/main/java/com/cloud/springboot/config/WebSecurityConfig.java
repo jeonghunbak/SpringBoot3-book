3d966fb606112ecd3595eb3afa38913fc5aadd9b
jeonghunbak <iiiiilllll@nate.com>
2023-06-14 23:42:44 +0900

    Blog Example - WebSecurityConfig

--- src/main/java/com/cloud/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/cloud/springboot/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.cloud.springboot.config;
 
+import com.cloud.springboot.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -11,6 +16,8 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
+
+    private final userDetailService userService;
 
     @Bean
     public WebSecurityCustomizer configure(){
@@ -36,5 +43,11 @@ public class WebSecurityConfig {
                 .and()
                 .csrf().disabled()
                 .bulid();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService)
     }
 }
