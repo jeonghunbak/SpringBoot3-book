061b3797704b1fe36fd39b7b47989e1d0a95e28b
jeonghunbak <iiiiilllll@nate.com>
2023-08-28 22:09:23 +0900

    Blog Example -  Commit

---
+++ src/main/java/com/cloud/springboot/domain/RefreshToken.java
@@ -0,0 +1,19 @@
+package com.cloud.springboot.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "userId", nullable = false, updatable = true)
+    private long userId
+}
