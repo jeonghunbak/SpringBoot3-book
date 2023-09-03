5e3242ca41de645f20595a05db29ac4b5dd23e68
jeonghunbak <iiiiilllll@nate.com>
2023-09-03 19:49:49 +0900

    Blog Example - Commit

--- src/main/java/com/cloud/springboot/service/TokenService.java
+++ src/main/java/com/cloud/springboot/service/TokenService.java
@@ -1,8 +1,11 @@
 package com.cloud.springboot.service;
 
 import com.cloud.springboot.config.jwt.TokenProvider;
+import com.cloud.springboot.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
@@ -12,4 +15,14 @@ public class TokenService {
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
 
+    public String createNewAccessToken(String refreshToken){
+        if(!tokenProvider.validToken(refreshToken)){
+            throw new IllegalArgumentException("unexpected Token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
 }
